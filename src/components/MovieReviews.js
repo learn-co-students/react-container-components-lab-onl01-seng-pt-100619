5c688cc1810744f5f2174d926bbb2dbc341aa432
Justin <justinphil02@yahoo.com>
2020-07-17 18:24:46 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+    const eachReview = (array) => array.map((review) => <li className="review" id="review">{review.summary_short}</li>)
+    return(
+        <ul className="review-list">
+            {eachReview(props.reviews)}
+        </ul>
+    )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends React.Component {
+
+    state = {
+        reviews: [],
+        searchTerm: ""
+    }
+
+    render() {
+        return (
+          <div className="searchable-movie-reviews">
+            <h2>Search for Movie Reviews</h2>
+            <form onSubmit={this.handleSubmit}>              
+              <input type="text" onChange={this.handleSearchInputChange} />
+              <button type="submit">Submit</button>
+            </form>
+            
+            <MovieReviews reviews={this.state.reviews} />
+          </div>
+        )
+    }   
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        fetch('https://api.nytimes.com/svc/movies/v2/reviews/search.json?dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ' + (this.state.searchTerm))
+        .then(res => res.json())
+        .then(res => this.setState({ reviews: res.results }));
+    }
+
+    handleSearchInputChange = (event) => {
+        this.setState({ searchTerm: event.target.value })
+    }
+}
